37d114b040fc7708ecf7892fa4a84b4ca203e1ba
Jerneja <bozic.jerneja@gmail.com>
2022-12-05 00:43:49 +0000

    Add spiral circle.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import argparse
 from shapes.animals import StaticTurtle
+from shapes.circles import SpiralCircle
 from shapes.flags import UkraineFlag
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-turtle", "--turtle", action="store_true", help="Draw a turtle")
 parser.add_argument("-ukr", "--ukraine", action="store_true", help="Draw Ukrainian flag")
+parser.add_argument("-circle", "--circle", action="store_true", help="Draw Spiral Circle")
 
 args = parser.parse_args()
 config = vars(args)
@@ -16,3 +18,5 @@ if config.get("turtle"):
 elif config.get("ukraine"):
     UkraineFlag().execute()
 
+elif config.get("circle"):
+    SpiralCircle().execute()
---
+++ shapes/circles.py
@@ -0,0 +1,14 @@
+import turtle
+
+
+class SpiralCircle:
+    @staticmethod
+    def execute():
+        t = turtle.Turtle()
+
+        # taking radius of initial radius
+        r = 10
+
+        # Loop for printing spiral circle
+        for i in range(100):
+            t.circle(r + i, 45)
